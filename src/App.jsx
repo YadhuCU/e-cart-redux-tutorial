198901ced9c285b80265b08039d5355c1bb1d2bb
YadhuCU <yadhuakku003@gmail.com>
2023-12-28 14:02:45 +0530

    initial commit | added bootstrap theme

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="d-flex m-5 justify-content-between">
+        <h1 className="shadow-lg btn btn-light">Hello world</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
